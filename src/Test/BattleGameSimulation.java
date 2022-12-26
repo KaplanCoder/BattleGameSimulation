372888a0aaa57b4263d3a2c480133a426eabf433
KaplanCoder <kemalselcukkaplan@gmail.com>
2022-12-26 22:20:38 +0300

    comment updated

--- src/Test/BattleGameSimulation.java
+++ src/Test/BattleGameSimulation.java
@@ -7,7 +7,7 @@ public class BattleGameSimulation {
 
 
         public static void main(String[] args) throws Exception {
-            // Default player number is two.
+            // Default number of player is two.
             GameEngine e=new GameEngine(2);	//Based on the argument, simulation can be run with multiple players.
             e.init();
         }
